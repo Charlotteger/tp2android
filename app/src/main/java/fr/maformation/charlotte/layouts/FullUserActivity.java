a6b9fc2149798fff288dc1d99d5fd27be4715c94
Charlott <charlotte.gerber@etud.univ-jfc.fr>
2018-10-09 15:48:29 +0200

    data binding

--- app/src/main/java/fr/maformation/charlotte/layouts/FullUserActivity.java
+++ app/src/main/java/fr/maformation/charlotte/layouts/FullUserActivity.java
@@ -1,21 +1,24 @@
 package fr.maformation.charlotte.layouts;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import fr.maformation.charlotte.layouts.databinding.FullUserActivityBinding;
 
 public class FullUserActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.full_user_activity);
-
+        FullUserActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.full_user_activity);
         User user = (User) getIntent().getSerializableExtra("user");
-        TextView nom = findViewById(R.id.nom); //champ nom
+        binding.setUser(user);
+        /*TextView nom = findViewById(R.id.nom); //champ nom
         TextView prenom = findViewById(R.id.prenom); //champ prenom
         TextView age = findViewById(R.id.age); //champ age
 
         nom.setText(user.getNom());
         prenom.setText(user.getPrenom());
-        age.setText(String.valueOf(user.getAge()));
+        age.setText(String.valueOf(user.getAge()));*/
     }
 }
